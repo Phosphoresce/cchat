82585b7529a6051adbc98a85a2d97bc1a7c0d5da
phosphoresce <phosphoresce.k@gmail.com>
2016-11-18 14:18:49 -0600

    wow this is dirty.

--- chatServer.c
+++ chatServer.c
@@ -1,3 +1,6 @@
+#define _XOPEN_SOURCE 500
+#define BUFF_SIZE 256
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,7 +10,16 @@
 #include <unistd.h>
 #include <netdb.h>
 
-#define BUFF_SIZE 255
+struct message {
+	int cid;
+	char s1[BUFF_SIZE];
+	char s2[BUFF_SIZE];
+};
+
+struct user {
+	int active;
+	struct sockaddr_in addr;
+};
 
 /*
  * Function: main
@@ -18,14 +30,18 @@
  *   returns 0 on success, 1 on failure
  */
 int main(int argc, char **argv) {
+	int c = 1;
+	int i;
 	int sockfd;
 	int debug = 0;
+	int currcid = 1;
 	int recvlen;
 	unsigned int port = 0;
-	char buffer[BUFF_SIZE];
+	char hostname[BUFF_SIZE];
 
-	struct sockaddr_in server;
-	struct sockaddr_in client;
+	struct sockaddr_in server, client;
+	struct message serv_msg, client_msg;
+	struct user usertable[10];
 	socklen_t addrlen = sizeof(client);
 
 	/* runtime args */
@@ -37,12 +53,18 @@ int main(int argc, char **argv) {
 		port = atoi(argv[1]);
 		debug = atoi(argv[2]);
 	}
+	for (i = 0; i <= 10; i++) {
+		usertable[i].active = 0;
+	}
 
 	/* connection settings */
+	gethostname(hostname, BUFF_SIZE);
 	memset((char *) &server, 0, sizeof(server));
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = htonl(INADDR_ANY);
 	server.sin_port = htons(port);
+
+	printf("%s: waiting for data on port UDP %d\n", argv[0], port);
 
 	/* create socket */
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -53,17 +75,62 @@ int main(int argc, char **argv) {
 	/* bind */
 	if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
 		printf("Failed to bind to socket.\n");
+		close(sockfd);
 		return 1;
 	}
 
-	/* recv */
-	recvlen = recvfrom(sockfd, buffer, BUFF_SIZE, 0, (struct sockaddr *)&client, &addrlen);
-	if (recvlen > 0) {
-		buffer[recvlen] = 0;
-		if (debug) {
-			printf("client: %s\n", buffer);
+	for (;;) {
+		/* recv */
+		recvlen = recvfrom(sockfd, &client_msg, sizeof(client_msg), 0, (struct sockaddr *) &client, &addrlen);
+		if (recvlen > 0) {
+			if (debug) {
+				printf("DEBUG: Recieving <%d %s %s>\n", client_msg.cid, client_msg.s1, client_msg.s2);
+			}
+		} else {
+			printf("cannot receive QUIT_ACK\n");
+			close(sockfd);
+			return 1;
 		}
+
+		/* compose */
+		if(!strcmp(client_msg.s1, "JOIN")) {
+			/* gen unused cid */
+			while (c) {
+				printf("%d\n", currcid);
+				if (usertable[currcid].active == 0) {
+					usertable[currcid].addr = client;
+					usertable[currcid].active = 1;
+					currcid++;
+					c = 0;
+				} else {
+					currcid++;
+				}
+			}
+			c = 1;
+
+			serv_msg.cid = currcid-1;
+			printf("%d\n", currcid);
+			strcpy(serv_msg.s1, client_msg.s1);
+			strcpy(serv_msg.s2, hostname);
+		} else if (!strcmp(client_msg.s1, "QUIT")) {
+			/* mark cid unused */
+			serv_msg.cid = abs(client_msg.cid);
+			usertable[client_msg.cid].active = 0;
+			strcpy(serv_msg.s1, client_msg.s1);
+			strcpy(serv_msg.s2, hostname);
+		} else {
+			/* send to every cid but senders */
+			serv_msg.cid = client_msg.cid;
+			strcpy(serv_msg.s1, hostname);
+			strcpy(serv_msg.s2, client_msg.s2);
+		}
+
+		/* send */
+		if (debug) {
+			printf("DEBUG: Sending <%d %s %s>\n", serv_msg.cid, serv_msg.s1, serv_msg.s2);
+		}
+		sendto(sockfd, &serv_msg, sizeof(serv_msg)+1, 0, (struct sockaddr *) &client, sizeof(client));
 	}
-	sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&client, addrlen);
+	close(sockfd);
 	return 0;
 }
